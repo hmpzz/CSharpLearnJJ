d8d136614c9226329663689ad74758163d02c4fc
yuqing zhou <969243585@qq.com>
2022-11-16 23:53:25 +0800

    +

---
+++ Lesson22_特殊语法/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace Lesson22_特殊语法
+{
+
+    class Person
+    {
+        private int money;
+        public bool sex;
+
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person(int _money)
+        {
+            money = _money;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson22_特殊语法");
+
+            #region 知识点一 var隐式类型
+            //var是一种特殊的变量美型
+            //它可以用来表示任意类型的变量
+            //注意：
+            //1.var不能作为类的成员只能用于临时变量申明时
+            //也就是一般写在函数语句块中
+            //2.var必须初始化
+            #endregion
+
+            #region 知识点二 设置对象初始值
+            //申明对象时
+            //可以通过直接写大括号的形式初始化公共成员变量和属性
+
+            Person p = new Person(100) { sex = true, age = 10, name = "aaa" };
+            #endregion
+
+            #region 知识点三 设置集合初始值
+            //申明集合对象时
+            //可以通过直接写大括号的形式初始化内部属性
+
+            int[] array2 = new int[] { 1, 2, 3, 4, 5 };
+            List<int> listInt = new List<int>() { 1, 2, 34, 5 };
+
+            List<Person> people = new List<Person>() { new Person(10) { age = 10 }, new Person(100) { sex = true, name = "fdsa" } };
+
+            Dictionary<string, int> dic = new Dictionary<string, int>() { { "1", 123 }, { "a", 222 } };
+            #endregion
+
+            #region 知识点四 匿名类型
+            //var 变量可以申明为自定义的匿名类型
+            var v = new { age = 10, money = 100, name = "fdsa" };
+
+            #endregion
+
+
+            #region 知识点五 可空类型
+            //1.值类型是不能赋值为空的
+            //int c = null;
+            //2.申明时在值类型后面加?可以赋值为空
+            int? c = null;
+
+            //3.判断是否为空
+            if (c.HasValue)
+            {
+                Console.WriteLine(c);
+            }
+            //4.安全获取可空类型值
+            //4-1.如果为空默认返回值类型的默认值
+            int? value = null;
+
+            //4-2.也可以指定一个默认值
+            Console.WriteLine(value.GetValueOrDefault(100));//GetValueOrDefault(可以指定默认值)
+
+            object o = null;
+
+            if (o != null)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            //相当于一种语法糖 能够帮我们判断o是不是空
+            //如果o是空也不会执行tostring,防止报错
+            Console.WriteLine(o?.ToString());
+
+            Action action = null;
+            action?.Invoke();
+
+            #endregion
+
+            #region 知识点六 空合并操作符
+            //空合并操作符??
+            //左边值??右边值
+            //如果左边值为null就返回右边值 否则返回左边值
+            //只要是可以为null的类型都能用
+            int? intV = null;
+            int intI = intV == null ? 100 : intV.Value;
+
+            intI = intV ?? 100;
+
+            #endregion
+
+            #region 知识点七 内插字符串
+            //关键符号 $
+            //用$来禁猎地字符串，让字符串中可以拼接变量
+            string sk = $"{123},{456}";
+            #endregion
+
+            #region 知识点八 单句逻辑的简略写法
+            
+            //if,for,while只有一句时可以省略大括号
+            //类的属性如果只有一句也可以写 set=>name="张三";
+            //类的方法如果只有一句也可以写成 public int Add(int x,int y )=> x+y;
+            #endregion
+
+        }
+    }
+
+
+}
---
+++ Lesson22_迭代器/Program.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+
+namespace Lesson22_迭代器
+{
+    internal class Program
+    {
+        #region 知识点一 迭代器是什么
+        //迭代器(iterator) 时又称光标(cursor)
+        //是程序设计的软件设计模式
+        //迭代器模式提供一个方法顺序访问一个聚合对象中的各个元素
+        //而又不暴露其内部的标识
+        //在表现效果上看
+        //是可以在容器对象(例如链表或数组)上遍历访问的接口
+        //设计人员无需关心容器对象的内存分配的实现细节
+        //可以用foreach遍历的类， 都是实现了迭代器的
+        #endregion
+
+        #region 知识点二 标准迭代器的实现方法
+        //关键接口：IEnumerator， IEnumerable
+        //命名空间：using System.Collections；
+        //可以通过同时继承IEnumerable和IEnumerator实现其中的方法
+
+        class CustomList : IEnumerable, IEnumerator
+        {
+            private int[] list;
+
+            private int position = -1;//这个是给movenext方法用的,用于表示数据到了哪个位置
+            public CustomList()
+            {
+                list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            }
+
+
+            #region IEnumerable
+            public IEnumerator GetEnumerator()
+            {
+
+                Reset();
+                //return list.GetEnumerator();
+                return this;
+                //throw new NotImplementedException();
+            }
+
+            #endregion
+
+            #region IEnumerator
+            public object Current
+            {
+                get { return list[position]; }
+            }
+
+
+            public bool MoveNext()
+            {
+                position++;
+
+                //判断是否溢出
+                return position < list.Length;
+
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region 知识点三 用yield return 语法糖实现迭代器
+        //yield return是c#提供给我们的语法糖
+        //所谓语法糖，也称糖衣语法
+        //主要作用就是将复杂逻辑简单化，可以增加程序的可读性
+        //从而减少程序代码出错的机会
+
+        //关键接口：IEnumerable
+        //命名空间：using System.Collections；
+        //让想要通过for each遍历的自定义类实现接口中的方法GetEnumerator即可
+
+        class CustomList2 : IEnumerable
+        {
+            private string[] list;
+
+            public CustomList2()
+            {
+                list = new string[] { "a", "b", "c","d","e","f"};
+            }
+            public IEnumerator GetEnumerator()
+            {
+                for (int i = 0; i < list.Length; i++)
+                {
+                    //yield关键字 配合迭代器使用
+                    //可以理解为 暂时返回保留当前的状态
+                    //一会还会再回来
+
+                    //C#语法糖
+                    yield return list[i];
+                }
+
+
+            }
+        }
+        #endregion
+
+        #region 知识点四 用yield return 语法糖为泛型类实现迭代器
+        class customList<T> : IEnumerable
+        {
+            private T[] array;
+
+            public customList(params T[] _array)
+            {
+                array = _array;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    yield return array[i];
+                }
+            }
+        }
+
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson22_迭代器");
+
+            CustomList customList = new CustomList();
+            
+            //foreach 本质
+            //1.先获取in后面的那个对象的 IEnumerator
+            //  会调用对象其中的GetEnumerator方法 来获取
+            //2.执行得到这个IEnumerator对象中的 MoveNext方法
+            //3.只要这个MoveNext方法的返回值是true 就会去得到Current
+            //  然后赋值给item
+            foreach (int item in customList)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in customList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("**************************");
+            CustomList2 customList2 = new CustomList2();
+            foreach (string item in customList2)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (string item in customList2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("**************************");
+
+            customList<string> list2 = new customList<string>("123","a","fdsfds","pppp");
+
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
